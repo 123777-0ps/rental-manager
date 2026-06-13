faa1609b4234cce5eef19c4e75439b3ae876f528
123777-0ps <123060044+123777-0ps@users.noreply.github.com>
2026-06-14 02:44:12 +0800

    Add Vercel serverless function entry point

---
+++ api/index.js
@@ -0,0 +1,7 @@
+﻿// Vercel Serverless Function - API entry point
+// Imports the Express app from the built server bundle
+
+const { app } = require('../dist-server/server.js');
+
+// Export the Express app for Vercel
+module.exports = app;
